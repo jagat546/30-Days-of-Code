cb7a9a554c8f6c809725143ca6390a4fefd588cf
Jagat Jyoti Mishra <59306207+jagat546@users.noreply.github.com>
2021-01-05 07:49:54 +0530

    Add files via upload

---
+++ Day0.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() 
+{
+    string inputString;
+    /* get a line of input from cin and save it to our variable*/
+    getline(cin, inputString); 
+
+    /* Your first line of output goes here*/
+    cout << "Hello, World." << endl;
+
+    /* Write the second line of output*/
+       
+    /* Solution starts from here */
+    
+    cout << inputString << endl;
+    
+    /* Solution ends here */
+    return 0;
+}
